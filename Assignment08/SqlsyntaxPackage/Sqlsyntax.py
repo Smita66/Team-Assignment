82ff2b36a1159ee42c5f95896c22ca6e9e360fba
smita <smita@DESKTOP-L3NU2O8.lan>
2024-03-27 22:05:17 -0400

    printing the output code

--- Assignment08/SqlsyntaxPackage/Sqlsyntax.py
+++ Assignment08/SqlsyntaxPackage/Sqlsyntax.py
@@ -1,11 +1,25 @@
 #sqlsyntax.py
+# Name: Rhodas Yemaneab and Smita Magar
+# email: yemanert@mail.uc.edu , diswamsa@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: 3/28/24
+# Course/Section: IS4010 001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This assignment demonstrates how to collaborate with team members to 
+# contrive a select query from SSMS and eclipse to sort and analyze the data. This assignment also demonstrated 
+# how to push and commit, and pull from Git hub when collaborating.   
+# Brief Description of what this module does.On this module, we create a function of SQL syntax using
+# group by, order by, and sum operations.
+# Citations: https://www.w3schools.com/sql/sql_groupby.asp
+# Anything else that's relevant: 
 
 
 def sql_select():
-    sql_select= "select SUM(td.AmountOff) as totalAmtOff, ts.CouponSource from tCoupon as tc \
-                 inner join tCouponDetail as td on tc.CouponID = td.CouponID    inner join \
-                 tCouponSource as ts on tc.CouponSourceID = ts.CouponSourceID \
-                 where td.PercentageDiscount >= 50 \
-                 group by ts.CouponSource \
-                 ORDER by totalAmtOff DESC"
+    sql_select= "SELECT SUM(td.AmountOff) as totalAmtOff, ts.CouponSource FROM tCoupon as tc \
+                 INNER JOIN tCouponDetail as td ON tc.CouponID = td.CouponID    INNER JOIN \
+                 tCouponSource as ts ON tc.CouponSourceID = ts.CouponSourceID \
+                 WHERE td.PercentageDiscount >= 50 \
+                 GROUP BY ts.CouponSource \
+                 ORDER BY totalAmtOff DESC"
     return sql_select;             
+
--- Assignment08/mainPackage/main.py
+++ Assignment08/mainPackage/main.py
@@ -1,21 +1,41 @@
 # main.py
 
 # Name: Rhodas Yemaneab and Smita Magar
-# email: yemanert@mail.uc.edu
+# email: yemanert@mail.uc.edu ,diswamsa@mail.uc.edu
 # Assignment Number: Assignment 08
 # Due Date: 3/28/24
 # Course/Section: IS4010 001
 # Semester/Year: Spring 2024
-# Brief Description of the assignment: 
-# Brief Description of what this module does. Do not copy/paste from a previous assignment. 
-# Citations: 
+# Brief Description of the assignment: This assignment demonstrates how to collaborate with team members to 
+# contrive a select query from SSMS and eclipse to sort and analyze the data. This assignment also demonstrated 
+# how to push and commit, and pull from Git hub when collaborating.   
+# Brief Description of what this module does: This module call the SQL function which has SQL syntax . it also
+# print the interesting fact about coupon discount which is analyzed after executing the SQL statements.
+# Citations:https://www.freecodecamp.org/news/print-newline-in-python/
+# https://www.geeksforgeeks.org/how-to-connect-python-with-sql-database/
 # Anything else that's relevant: 
 
 import pyodbc
+from SqlsyntaxPackage.Sqlsyntax import sql_select
 conn = pyodbc.connect('Driver={SQL Server};'
                         'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
                         'Database=GroceryStoreSimulator;'
                         'uid=IS4010Login;'
                         'pwd=P@ssword2;')
 cursor = conn.cursor()
-cursor.execute( )
+# Execute the SQL query from sqlsyntax.py
+sql_query = sql_select()
+cursor.execute(sql_query)
+
+totalAmountOff = list();
+couponsSource = list();
+# Fetch and print the results if needed
+for row in cursor.fetchall():
+    print("-------------")
+    print(row[0] ,"|", row[1])
+    totalAmountOff.append(row[0]);
+    couponsSource.append(row[1]);
+#printing the output of code in one sentence.
+print("\n")  
+print("The highest coupon discount is %s with CouponSource: %s and the lowest coupon discount is %s with CouponSource: %s " 
+      %(totalAmountOff.pop(0), couponsSource.pop(0), totalAmountOff.pop(len(totalAmountOff)-1), couponsSource.pop(len(couponsSource)-1)));
